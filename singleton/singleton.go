9e22c0be506701aed26b7e72ff9107aa4aa816cd
咏鹅 <cocoww595@gmail.com>
2023-09-17 21:21:40 +0800

    init design

--- singleton/singleton.go
+++ singleton/singleton.go
@@ -31,3 +31,26 @@ func GetLazySingleton() *Singleton {
 	}
 	return instance
 }
+
+// GetDoubleCheckInstance 双重锁单例
+func GetDoubleCheckInstance() *Singleton {
+	if instance == nil {
+		mu.Lock()
+		defer mu.Unlock()
+		if instance == nil {
+			instance = &Singleton{
+				Data: "双重锁单例数据",
+			}
+		}
+	}
+	return instance
+}
+
+// 饿汉
+var eInstance = &Singleton{
+	Data: "饿汉单例数据",
+}
+
+func GetEagerInstance() *Singleton {
+	return eInstance
+}
